5413b7304cfd6b6c970089958441129c18028189
zhuicode99 <enlin291814833@hotmail.com>
2022-08-23 00:15:58 +0000

    use interpolation  in this function

--- assertEqual.js
+++ assertEqual.js
@@ -10,9 +10,9 @@ const assertEqual = function(actual, expected) {
 //new function with emoji
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😍😍😍" + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("😔😔😔" + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
