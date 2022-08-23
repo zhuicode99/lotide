14a03dd715b639702746468c18470e97627b13c1
zhuicode99 <enlin291814833@hotmail.com>
2022-08-23 00:08:27 +0000

    use emoji in this function

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+/* // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+}; */
+
+//new function with emoji
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😍😍😍" + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😔😔😔" + "Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
