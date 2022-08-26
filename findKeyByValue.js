2273844099ab3aaec33969d77201018a7f0a7d2d
zhuicode99 <enlin291814833@hotmail.com>
2022-08-26 00:44:14 +0000

    first edition findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+//assertEqual function;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+//Actual Code:
+
+const findKeyByValue = function(object, value){
+  for (let key of Object.keys(object)) {
+    if( object[key] === value){
+    return key;
+    }
+  }
+};
+
+//TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
