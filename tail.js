d940cca692d569bf085ba6fcf473b7801fb261e5
zhuicode99 <enlin291814833@hotmail.com>
2022-08-29 20:51:22 +0000

    refactor

--- tail.js
+++ tail.js
@@ -1,15 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(arr) {
   let tailNum = arr.slice(1);
   return (arr, tailNum);
-};
-
-const assertEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] === expected[i] && actual.length === expected.length) {
-      return console.log(`😍😍😍Assertion Passed: ${actual} === ${expected}`);
-    }
-  }
-  return console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
