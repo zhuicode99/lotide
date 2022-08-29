3e50de9d710cfc09bba9c6bd67d7902a99dbd6bd
zhuicode99 <enlin291814833@hotmail.com>
2022-08-29 23:52:23 +0000

    refactor

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-//eqArrays function
-const eqArrays = function(arr1, arr2)  {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//assertArraysEqual function
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("True, these two arrays are equal");
-  } else {
-    console.log("False, these two arrays aren't equal");
-  }
-};
-
-
 //Actual Function
 
 const middle = function(array) {
